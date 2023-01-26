2e1b493da43758442231d25f6fae22bb7eb9735c
Beatricia Teh <58943667+Biiiiiiiiiiiii@users.noreply.github.com>
2023-01-26 19:05:21 +0800

    Update fire_movement.py

--- webots-fire-scene-master/controllers/fire_movement/fire_movement.py
+++ webots-fire-scene-master/controllers/fire_movement/fire_movement.py
@@ -1,33 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 29 22∶26∶14 2020
 
-@author: Angel Ayala <angel4ayala [at] gmail.com>
-"""
 
 from controller import Robot
 
 
 class FireMovement(Robot):
-    """First approach of fire simulation in Webots.
-
-    Implements 2 Display nodes to present the fire with smoke. It is included
-    with the FireSmoke.proto file.
-    """
-
+    
     def __init__(self, fps=25):
-        """Initialize FireMovement controller for the FireSmoke Node.
-
-        This class instantiate the Display Nodes to simulate a flame and smoke
-        movement using sprites images at the desired fps to be captured by
-        image sensors devices as a Camera.
-
-        Parameters
-        ----------
-        fps : int, optional
-            The desired frame per seconds's rate. The default is 25.
-        """
+       
         super(FireMovement, self).__init__()
         # self.timeStep = int(self.getBasicTimeStep())
         # set the control time step
@@ -38,12 +17,11 @@ class FireMovement(Robot):
         print('OK')
 
     def init_displays(self):
-        """Initialize the fire and smoke Display nodes."""
+       
         self.fire_screen = self.getDisplay("fireDisplay")
         self.smoke_screen = self.getDisplay("smokeDisplay")
 
     def load_sprites(self):
-        """Load and configure the sprite cheet of fire and smoke."""
         self.fire_img = self.fire_screen.imageLoad(
             "320x320_fire_sprint.png")
         self.smoke_img = self.smoke_screen.imageLoad(
@@ -61,13 +39,11 @@ class FireMovement(Robot):
         self.n_smoke_frames = len(self.smoke_frame_xy)
 
     def move_fire(self, step=-1):
-        """Change the displayed sprite in the fire Display node."""
         frame = step % self.n_fire_frames
         pos = self.fire_frame_xy[frame]
         self.fire_screen.imagePaste(self.fire_img, pos[0], pos[1], False)
 
     def move_smoke(self, step=-1):
-        """Change the displayed sprite in the smoke Display node."""
         # ts = step -5 if step > 11 else step
         # if step <= 11:
         # 	smoke_xy = smoke_frame_xy
@@ -80,10 +56,6 @@ class FireMovement(Robot):
         self.smoke_screen.imagePaste(self.smoke_img, pos[0], pos[1], False)
 
     def run(self):
-        """Run controller's main loop.
-
-        Change the sprite presented in the Display nodes.
-        """
         # main control loop:
         print('Running...')
         counter = 1
